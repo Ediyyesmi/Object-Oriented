c7b8726fb00e8fe98970788734861212f8217e9a
Ediyyesmi <143936492+Ediyyesmi@users.noreply.github.com>
2024-11-13 14:42:48 +0300

    .

---
+++ static.java
@@ -0,0 +1,31 @@
+//use static keyword
+
+public class Student{
+    public String name;
+    public int number;
+    private static int counter=0;
+
+    Student(String name, int number){
+        this.name=name;
+        this.number=number;
+        Student.counter++;
+    }
+
+    public static int howmuch(){
+        return Student.counter;
+    }
+
+    public void exit(){
+        Student.counter--;
+    }
+
+    public static void main(String[] args){
+        Student s1 = new Student("Seyyide", 100);
+        System.out.println("number of student: "+Student.howmuch());
+        Student s2 = new Student("Nur", 97);
+        System.out.println("number of student: "+Student.howmuch());
+        s1.exit();
+        System.out.println("number of student :"+Student.howmuch());
+
+    }
+}
